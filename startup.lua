1308a8cbf53c41626a21cf79e5d4877a5c272f37
Dotmazy <95581415+Dotmazy@users.noreply.github.com>
2024-02-09 23:22:03 +0100

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -3,11 +3,12 @@ term.setCursorPos(10,10)
 term.write("A - Start Dot OS")
 term.setCursorPos(10,11)
 term.write("B - Start Craft OS")
+
 while true do
   local event, char = os.pullEvent("char")
   if char == "a" then
     shell.run("main")
-  else if char == "b" then
+  elseif char == "b" then
     break
   end
 end
