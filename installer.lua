796677935c8127be2c61f303d0ace4eb3d68e2e0
Dotmazy <95581415+Dotmazy@users.noreply.github.com>
2024-02-09 22:36:06 +0100

    Create installer.lua

---
+++ installer.lua
@@ -0,0 +1,30 @@
+local function makeFile(path, text)
+  local file = io.open(path,"w")
+  file:write(text)
+  file:close()
+end
+
+local function get(name, url)
+  if fs.exists(name) then
+    fs.delete(name)
+  end
+
+  print("Downloading file '"..name.."' from "..url)
+
+  makeFile(name,http.get(url).readAll())
+end
+
+get("version","https://raw.githubusercontent.com/Dotmazy/Web-Craft/main/index.webc")
+
+print("\nThe computer needs to be restarted in order to apply the changes.")
+print("(press any key to continue)")
+os.pullEvent("key")
+local i=0
+local x,y = term.getCursorPos()
+while i<6 do
+  i=i+1
+  term.setCursorPos(x,y)
+  term.write("Rebooting computer "..string.rep(".",(i-1)%3+1)..string.rep(" ",3-((i-1)%3+1)))
+  sleep(0.5)
+end
+os.reboot()
